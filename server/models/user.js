90aa49963ec468d8bd77c9943a0f7df38383f429
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-05-27 09:40:27 +0900

    fix user.js require => required

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: {type: String, require: true},
-  email: {type: String, unique: true, require: true},
+  email: {type: String, unique: true, required: true},
   password: {type: String, require: true},
 })
 
